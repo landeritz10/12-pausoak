7402a0cb96bcf3a18a8cdf3a2ded18ef716d4e06
landeritz10 <landeritz10@gmail.com>
2026-03-02 23:29:19 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,13 @@
+input.onPinPressed(TouchPin.P0, function () {
+	
+})
+input.onButtonPressed(Button.A, function () {
+    MUESTRA_NÚMERO_1 = 0
+})
+let MUESTRA_NÚMERO_1 = 0
+basic.showString("KAIXO")
+basic.pause(500)
+basic.showIcon(IconNames.Ghost)
+basic.forever(function () {
+	
+})
